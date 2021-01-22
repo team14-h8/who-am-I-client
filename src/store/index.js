13963096350d144b44ca8247bf1c7eef96554f6e
ical10 <mochhusnir@gmail.com>
2021-01-22 09:46:26 +0700

    replacing double quotes with single quotes

--- src/store/index.js
+++ src/store/index.js
@@ -7,31 +7,31 @@ export default new Vuex.Store({
   state: {
     soal: [
       {
-        image: "https://i.ibb.co/SvS3F0z/najwa-pixelated.png",
-        answer: "najwa shihab"
+        image: 'https://i.ibb.co/SvS3F0z/najwa-pixelated.png',
+        answer: 'najwa shihab'
       },
       {
-        image: "https://i.ibb.co/KsZz0bj/jokowi-pixelated.png",
-        answer: "joko widodo"
+        image: 'https://i.ibb.co/KsZz0bj/jokowi-pixelated.png',
+        answer: 'joko widodo'
       },
       {
-        image: "https://i.ibb.co/VpCCG6q/anya-pixelated.png",
-        answer: "anya geraldine"
+        image: 'https://i.ibb.co/VpCCG6q/anya-pixelated.png',
+        answer: 'anya geraldine'
       },
       {
-        image: "https://i.ibb.co/QNrPhpK/ahmad-dhani-pixelated.png",
-        answer: "ahmad dhani"
+        image: 'https://i.ibb.co/QNrPhpK/ahmad-dhani-pixelated.png',
+        answer: 'ahmad dhani'
       },
       {
-        image: "https://i.ibb.co/T8HPG6B/agnes-mo-pixelated.png",
-        answer: "agnes monica"
+        image: 'https://i.ibb.co/T8HPG6B/agnes-mo-pixelated.png',
+        answer: 'agnes monica'
       },
       {
-        image: "https://i.ibb.co/ynkRNR5/raff-ahmad-pixelated.png",
-        answer: "raffi ahmad"
+        image: 'https://i.ibb.co/ynkRNR5/raff-ahmad-pixelated.png',
+        answer: 'raffi ahmad'
       }
     ],
-    
+
     users: []
   },
   mutations: {
